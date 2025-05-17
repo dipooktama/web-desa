5c304649262050f4bf737665100fbfa39b3cb694
dipooktama <dipooktama@usu.ac.id>
2025-05-17 12:48:09 +0700

    test layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { Alert } from "flowbite-svelte";
+</script>
+
+<div class="p-8">
+    <h1>Hello World!</h1>
+</div>
