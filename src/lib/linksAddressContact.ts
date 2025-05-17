1d06cc69b80f50a8b63799bc20487485e1d11ad5
dipooktama <dipooktama@usu.ac.id>
2025-05-17 19:15:46 +0700

    update address links

--- src/lib/linksAddressContact.ts
+++ src/lib/linksAddressContact.ts
@@ -35,36 +35,36 @@ interface Url {
 }
 
 export const Address: GovernmentAddress = {
-    name: "Pemerintah Desa Kersik",
+    name: "Pemerintah Desa Hibun",
     street: "Jalan Langaseng Dusun Empang RT.003",
-    village: "Desa Kersik",
-    district: "Kecamatan Marang Kayu",
-    city: "Kabupaten Kutai Kartanegara",
-    province: "Provinsi Kalimantan Timur",
-    postal_code: "75385"
+    village: "Desa Hibun",
+    district: "Kecamatan Parindu",
+    city: "Kabupaten Sanggau",
+    province: "Provinsi Kalimantan Barat",
+    postal_code: "78561"
 }
 
 export const Contacts: Contact[] = [
     {type: ContactType.PHONE, contact: "082150208664"},
-    {type: ContactType.EMAIL, contact: "kersik.marangkayu@kukarkab.go.id"}
+    {type: ContactType.EMAIL, contact: "hibun.parindu@sanggaukab.go.id"}
 ]
 
 export const PersonalContacts: Contact[] = [
-    {type: ContactType.PHONE, label:"Jumadi/Kades Kersik", contact: "081242368478"},
-    {type: ContactType.PHONE, label:"Yayan/Ambulan Kersik", contact: "085392095123"}
+    {type: ContactType.PHONE, label:"Jumadi/Kades Hibun", contact: "081242368478"},
+    {type: ContactType.PHONE, label:"Yayan/Ambulan Hibun", contact: "085392095123"}
 ]
 
 export const Socials: Contact[] = [
-    {type: ContactType.SOCIAL, social_media_type: SocialMediaType.IG, contact:"https://www.instagram.com/kersik.desa.id"},
+    {type: ContactType.SOCIAL, social_media_type: SocialMediaType.IG, contact:"https://www.instagram.com/hisbun"},
     {type: ContactType.SOCIAL, social_media_type: SocialMediaType.FB, contact:"https://www.facebook.com/profile.php?id=100087087031705"},
-    {type: ContactType.SOCIAL, social_media_type: SocialMediaType.X, contact:"https://mobile.twitter.com/pemdeskersik"},
-    {type: ContactType.SOCIAL, social_media_type: SocialMediaType.YT, contact:"https://www.youtube.com/channel/UCKeq3qOKrdwvg05alm7zT5w"},
-    {type: ContactType.SOCIAL, social_media_type: SocialMediaType.TT, contact:"https://www.tiktok.com/@kersik.desa.id"}
+    {type: ContactType.SOCIAL, social_media_type: SocialMediaType.X, contact:"https://mobile.twitter.com/pemdeshisbun"},
+    {type: ContactType.SOCIAL, social_media_type: SocialMediaType.YT, contact:"https://www.youtube.com/channel/???"},
+    {type: ContactType.SOCIAL, social_media_type: SocialMediaType.TT, contact:"https://www.tiktok.com/@desa_hibun"}
 ]
 
 export const OuterLinks: Url[] = [
     {name: "Website Kemendesa", url: "https://kemendesa.go.id"},
     {name: "Website Kemendagri", url: "https://kemendagri.go.id"},
-    {name: "Website Kabupaten Kutai Kartanegara", url: "https://kukarkab.go.id/v2"},
+    {name: "Website Kabupaten Sanggau", url: "https://sanggaukab.go.id/v2"},
     {name: "Cek DPT Online", url: "https://cekdptonline.kpu.go.id"}
 ]
