956293d7a65aa8cd29f88f3c7d0c5d3a843a0e7b
dipooktama <dipooktama@usu.ac.id>
2025-05-17 12:47:52 +0700

    added layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import '../app.css';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 
 	let { children } = $props();
 </script>
 
+<Navbar></Navbar>
 {@render children()}
+<Footer />
