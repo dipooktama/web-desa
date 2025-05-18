643970ac012de6ee584ed5c4bb7939d8d92fdcdd
dipooktama <dipooktama@usu.ac.id>
2025-05-18 08:35:51 +0700

    add active and removed sticky for a while

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,17 +1,19 @@
 <script>
+    import { page } from "$app/state";
 	import { navMenu } from "../navMenu";
     import { Address } from "$lib/linksAddressContact";
     import { Navbar, NavBrand, NavLi, NavUl } from "flowbite-svelte";
+    let activeUrl = $derived(page.url.pathname)
 </script>
 
-<Navbar class="fixed start-0 top-0 z-20 w-full">
+<Navbar>
     <NavBrand href="/">
         <div class="flex flex-col justify-start text-gray-300 pl-6">
             <p class="font-black">{ Address.village }</p>
             <p>{ Address.district }</p>
         </div>
     </NavBrand>
-    <NavUl>
+    <NavUl {activeUrl}>
         {#each navMenu as navItem (navItem.path) }
             <NavLi href={navItem.path} class="text-gray-300">{navItem.name}</NavLi>
         {/each}
