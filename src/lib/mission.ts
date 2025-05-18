7258395576d0d581bb1d8d691b0363786ec95506
dipooktama <dipooktama@usu.ac.id>
2025-05-18 09:10:20 +0700

    update profile page

---
+++ src/lib/mission.ts
@@ -0,0 +1,7 @@
+export const VillageMission: string[] = [
+    "Mewujudkan tata kelola pemerintahan yang baik",
+    "Mengembangkan kegiatan keagamaan",
+    "Meningkatkan kualitas pendidikan dan sumber daya manusia",
+    "Mengembangkan teknologi informasi",
+    "Pembangunan infrastruktur, sarana dan prasarana",
+]
---
+++ src/routes/profil/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Section from "$lib/components/Section.svelte";
+    import { VillageMission } from "$lib/mission";
+</script>
+
+<main class="py-8">
+    <Section classNames="mx-auto w-1/2 px-8 py-6">
+        <h1 class="font-bold text-3xl text-primary-700 mb-4 text-center">Visi</h1>
+        <p class="text-pretty">
+            Desa Hibun sebagai Desa Wisata yang mampu mengelolah potensi Desa dan pembangunan berkelanjutan untuk mewujudkan masyarakat yang sejahtera
+        </p>
+    </Section>
+    <Section classNames="mx-auto w-1/2 px-8 py-6">
+        <h1 class="font-bold text-3xl text-primary-700 mb-4 text-center">Misi</h1>
+        <ol class="list-decimal px-2">
+            {#each VillageMission as mission }
+                <li class="mx-auto text-pretty">{mission}</li>
+            {/each}
+        </ol>
+    </Section>
+</main>
