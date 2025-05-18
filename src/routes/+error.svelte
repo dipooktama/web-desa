b85c335d3f7fe674b69b83e89a85519d7761e2d4
dipooktama <dipooktama@usu.ac.id>
2025-05-18 09:24:19 +0700

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { page } from "$app/state"; 
+</script>
+
+<main class="h-screen flex flex-col text-center justify-center">
+    <h1 class="text-3xl font-black">{page.status} Error</h1>
+    <p>{page.error?.message}</p>
+</main>
